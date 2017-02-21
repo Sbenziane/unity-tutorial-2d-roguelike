fe04a34c8a816355fb3fd6db85ff1af4dbe2aae0
Luke Armstrong <me@lukearmstrong.co.uk>
2017-02-21 21:06:13 +0000

    Writing the Game Manager and Loader scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance = null;
+	public BoardManager boardScript;
 
-	// Use this for initialization
-	void Start () {
-		
+	private int level = 3;
+
+	void Awake()
+	{
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
+		boardScript = GetComponent<BoardManager>();
+		InitGame();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void InitGame()
+	{
+		boardScript.SetupScene(level);
 	}
 }
