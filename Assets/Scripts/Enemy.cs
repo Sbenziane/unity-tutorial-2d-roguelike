050c2a5ad94e70ca4f973826b52658427b4a5778
Luke Armstrong <me@lukearmstrong.co.uk>
2017-02-22 15:51:09 +0000

    enemy animator controller

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,8 +12,11 @@ public class Enemy : MovingObject
 
 	protected override void Start()
 	{
+		GameManager.instance.AddEnemyToList(this);
+
 		animator = GetComponent<Animator>();
 		target = GameObject.FindGameObjectWithTag("Player").transform;
+
 		base.Start();
 	}
 
@@ -31,6 +34,8 @@ public class Enemy : MovingObject
 	protected override void OnCantMove <T> (T component)
 	{
 		Player hitPlayer = component as Player;
+
+		animator.SetTrigger("enemyAttack");
 
 		hitPlayer.LoseFood(playerDamage);
 	}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 	public static GameManager instance = null;
+
 	public BoardManager boardScript;
 	public int playerFoodPoints = 100;
 	[HideInInspector] public bool playersTurn = true;
+	public float turnDelay = .1f;
 
 	private int level = 3;
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
 
 	void Awake()
 	{
@@ -20,17 +24,59 @@ public class GameManager : MonoBehaviour
 		}
 
 		DontDestroyOnLoad(gameObject);
+
+		enemies = new List<Enemy>();
+
 		boardScript = GetComponent<BoardManager>();
+
 		InitGame();
 	}
 
 	void InitGame()
 	{
+		enemies.Clear();
+
 		boardScript.SetupScene(level);
+	}
+
+	void Update()
+	{
+		if (playersTurn || enemiesMoving) {
+			return;
+		}
+
+		StartCoroutine(MoveEnemies());
+	}
+
+	public void AddEnemyToList(Enemy script)
+	{
+		enemies.Add(script);
 	}
 
 	public void GameOver()
 	{
 		enabled = false;
+	}
+
+	IEnumerator MoveEnemies()
+	{
+		enemiesMoving = true;
+
+		yield return new WaitForSeconds(turnDelay);
+
+		if (enemies.Count == 0) {
+			yield return new WaitForSeconds(turnDelay);
+		}
+
+		for (int i = 0; i < enemies.Count; i++)
+		{
+			enemies[i].MoveEnemy();
+
+			yield return new WaitForSeconds(enemies[i].moveTime);
+		}
+			
+		enemiesMoving = false;
+
+		playersTurn = true;
 	}
 }
